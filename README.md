acf71158e786004b31129c6fc1833a46f126ba3f
ugeorge <ugeorge@kth.se>
2016-05-28 22:52:23 +0200

    minor documentation

--- README.md
+++ README.md
@@ -1,8 +1,47 @@
-forsyde-haskell-experimental
-============================
+forsyde-atom
+============
 
-Reimplementation of the ForSyDe-Haskell modelling library in an applicative manner.
+Reimplementation of the ForSyDe-Haskell modelling library in an
+applicative manner using the concept of atoms, and a layered process
+model.
+
+---- 
+
+Ideas based on the projects `forsyde-patterns` (George
+Ungureanu), `forsyde-appl` (Mikkel Jakobsen) and the `ForSyDe. library
+(KTH-SAM group)
 
 
-----
-Ideas based on the projects `forsyde-patterns` (George Ungureanu), `forsyde-appl` (Mikkel Jakobsen) and the `ForSyDe. library (KTH-SAM group)
+Installation and usage
+----------------------
+
+The project is equipped with a makefile. To install the Haskell
+library inside a sandbox type:
+
+    make sandbox
+
+To generate the haddock extended API + technical report documentation
+type:
+
+    make doc
+
+To clean the documentation and the intermediate files generated for it
+type:
+
+    make clean
+
+There are a number of dependencies to be taken care of:
+
+ * Haskell dependencies: check `forsyde-atom.cabal` for the libraries
+   and compilers necessary
+
+ * `git`: for setting up the right `type-level` and
+   `parameterized-data` in the sandbox.
+
+ * `haddock` for generating the documentation
+
+ * a LaTeX compiler (`pdflatex`) for generating the pictures inside
+   the documentation.
+
+ * `imagemagick` to convert the above PDF pictures to PNG to be
+   recognized by Haddock.
--- src/ForSyDe/MoCLib/SY.hs
+++ src/ForSyDe/MoCLib/SY.hs
@@ -1,18 +1,17 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  ForSyDe.MoC.SY
--- Copyright   :  (c) George Ungureanu, KTH/ICT/E 2015; 
---                    SAM Group, KTH/ICT/ECS 2007-2008
+-- Copyright   :  (c) George Ungureanu, KTH/ICT/E 2015-2016
 -- License     :  BSD-style (see the file LICENSE)
 -- 
 -- Maintainer  :  ugeorge@kth.se
 -- Stability   :  experimental
 -- Portability :  portable
 --
--- The synchronuous library defines process constructors, processes and a signal conduit
--- for the synchronous computational model. A process constructor is a
--- higher order function which together with combinational function(s)
--- and values as arguments constructs a process. 
+-- The SY library implements the atoms holding the sematics for the
+-- synchronous computation model. It also provides a set of helpers
+-- for properly instantiating process network patterns as process
+-- constructors.
 -----------------------------------------------------------------------------
 
 module ForSyDe.MoCLib.SY where
@@ -22,10 +21,17 @@ import qualified ForSyDe.Core.MoC as MoC
 import           ForSyDe.Core.Signal as S
 import           ForSyDe.Core.ValueExt hiding (value)
 
-data SY a  = SY a
+-- | Type alias for a SY signal
 type Sig a = S.Signal (SY (Value a))
+
+-- | The SY event. It identifies a synchronous signal and implemets an
+-- instance of the 'MoC' class. Since SY tags are implicit, this data
+-- type only wraps values inside a constructor that identifies it as a
+-- "synchronous event".
+data SY a  = SY a
 -----------------------------------------------------------------------------
 
+-- | Implenents the SY semantics for the MoC atoms.
 instance MoC SY where
   (-$-) = (<$>).(<$>)
   ---------------------
@@ -37,16 +43,21 @@ instance MoC SY where
   ---------------------
   (-&-) _ a = a
   ---------------------
-  
+
+-- | Shows the (extended) value wrapped
 instance Show a => Show (SY a) where
   showsPrec _ (SY x) = (++) (show x)
 
+-- | Reads the (extended) value wrapped
 instance Read a => Read (SY a) where
   readsPrec _ s       = [(SY x, r) | (x, r) <- reads s]
 
+-- | Needed for the implementation of the '-$-' atom and also the
+-- @unzip@ utilities.
 instance Functor SY where
   fmap f (SY a) = SY (f a)
 
+-- | Needed for the implementation of the '-*-' atom
 instance Applicative SY where
   pure = SY 
   (SY a) <*> (SY b) = SY (a b)
@@ -54,14 +65,18 @@ instance Applicative SY where
 
 -----------------------------------------------------------------------------
 
-value    = SY . Value
+-- | Wraps a base value into a SY event of extended values
+event    :: a -> SY (Value a)
+event    = SY . Value
 
+-- | Wraps a list into a SY signal
+signal   :: [a] -> Sig a
 signal l = S.signal ((SY . Value) <$> l)
 
 -----------------------------------------------------------------------------
 
 delay :: a -> Sig a -> Sig a
-delay i = MoC.delay (value i)
+delay i = MoC.delay (event i)
 
 comb11 :: (a1 -> b1)
        -> Sig a1 -> Sig b1                                
@@ -147,22 +162,22 @@ scanl43 :: ((b1, b2, b3) -> a1 -> a2 -> a3 -> a4 -> (b1, b2, b3)) -> (b1, b2, b3
 scanl44 :: ((b1, b2, b3, b4) -> a1 -> a2 -> a3 -> a4 -> (b1, b2, b3, b4)) -> (b1, b2, b3, b4)
         -> Sig a1 -> Sig a2 -> Sig a3 -> Sig a4 -> (Sig b1, Sig b2, Sig b3, Sig b4)
 
-scanl11 ns i = MoC.scanl11 (psi21 ns) (value i)
-scanl12 ns i = MoC.scanl12 (psi21 ns) (value i)
-scanl13 ns i = MoC.scanl13 (psi21 ns) (value i)
-scanl14 ns i = MoC.scanl14 (psi21 ns) (value i)
-scanl21 ns i = MoC.scanl21 (psi31 ns) (value i)
-scanl22 ns i = MoC.scanl22 (psi31 ns) (value i)
-scanl23 ns i = MoC.scanl23 (psi31 ns) (value i)
-scanl24 ns i = MoC.scanl24 (psi31 ns) (value i)
-scanl31 ns i = MoC.scanl31 (psi41 ns) (value i)
-scanl32 ns i = MoC.scanl32 (psi41 ns) (value i)
-scanl33 ns i = MoC.scanl33 (psi41 ns) (value i)
-scanl34 ns i = MoC.scanl34 (psi41 ns) (value i)
-scanl41 ns i = MoC.scanl41 (psi51 ns) (value i)
-scanl42 ns i = MoC.scanl42 (psi51 ns) (value i)
-scanl43 ns i = MoC.scanl43 (psi51 ns) (value i)
-scanl44 ns i = MoC.scanl44 (psi51 ns) (value i)
+scanl11 ns i = MoC.scanl11 (psi21 ns) (event i)
+scanl12 ns i = MoC.scanl12 (psi21 ns) (event i)
+scanl13 ns i = MoC.scanl13 (psi21 ns) (event i)
+scanl14 ns i = MoC.scanl14 (psi21 ns) (event i)
+scanl21 ns i = MoC.scanl21 (psi31 ns) (event i)
+scanl22 ns i = MoC.scanl22 (psi31 ns) (event i)
+scanl23 ns i = MoC.scanl23 (psi31 ns) (event i)
+scanl24 ns i = MoC.scanl24 (psi31 ns) (event i)
+scanl31 ns i = MoC.scanl31 (psi41 ns) (event i)
+scanl32 ns i = MoC.scanl32 (psi41 ns) (event i)
+scanl33 ns i = MoC.scanl33 (psi41 ns) (event i)
+scanl34 ns i = MoC.scanl34 (psi41 ns) (event i)
+scanl41 ns i = MoC.scanl41 (psi51 ns) (event i)
+scanl42 ns i = MoC.scanl42 (psi51 ns) (event i)
+scanl43 ns i = MoC.scanl43 (psi51 ns) (event i)
+scanl44 ns i = MoC.scanl44 (psi51 ns) (event i)
 
 
 moore11 :: (st -> a1 -> st) -> (st -> b1) -> st
@@ -198,22 +213,22 @@ moore43 :: (st -> a1 -> a2 -> a3 -> a4 -> st) -> (st -> (b1, b2, b3)) -> st
 moore44 :: (st -> a1 -> a2 -> a3 -> a4 -> st) -> (st -> (b1, b2, b3, b4)) -> st
         -> Sig a1 -> Sig a2 -> Sig a3 -> Sig a4 -> (Sig b1, Sig b2, Sig b3, Sig b4)
 
-moore11 ns od i = MoC.moore11 (psi21 ns) (psi11 od) (value i)
-moore12 ns od i = MoC.moore12 (psi21 ns) (psi12 od) (value i)
-moore13 ns od i = MoC.moore13 (psi21 ns) (psi13 od) (value i)
-moore14 ns od i = MoC.moore14 (psi21 ns) (psi14 od) (value i)
-moore21 ns od i = MoC.moore21 (psi31 ns) (psi11 od) (value i)
-moore22 ns od i = MoC.moore22 (psi31 ns) (psi12 od) (value i)
-moore23 ns od i = MoC.moore23 (psi31 ns) (psi13 od) (value i)
-moore24 ns od i = MoC.moore24 (psi31 ns) (psi14 od) (value i)
-moore31 ns od i = MoC.moore31 (psi41 ns) (psi11 od) (value i)
-moore32 ns od i = MoC.moore32 (psi41 ns) (psi12 od) (value i)
-moore33 ns od i = MoC.moore33 (psi41 ns) (psi13 od) (value i)
-moore34 ns od i = MoC.moore34 (psi41 ns) (psi14 od) (value i)
-moore41 ns od i = MoC.moore41 (psi51 ns) (psi11 od) (value i)
-moore42 ns od i = MoC.moore42 (psi51 ns) (psi12 od) (value i)
-moore43 ns od i = MoC.moore43 (psi51 ns) (psi13 od) (value i)
-moore44 ns od i = MoC.moore44 (psi51 ns) (psi14 od) (value i)
+moore11 ns od i = MoC.moore11 (psi21 ns) (psi11 od) (event i)
+moore12 ns od i = MoC.moore12 (psi21 ns) (psi12 od) (event i)
+moore13 ns od i = MoC.moore13 (psi21 ns) (psi13 od) (event i)
+moore14 ns od i = MoC.moore14 (psi21 ns) (psi14 od) (event i)
+moore21 ns od i = MoC.moore21 (psi31 ns) (psi11 od) (event i)
+moore22 ns od i = MoC.moore22 (psi31 ns) (psi12 od) (event i)
+moore23 ns od i = MoC.moore23 (psi31 ns) (psi13 od) (event i)
+moore24 ns od i = MoC.moore24 (psi31 ns) (psi14 od) (event i)
+moore31 ns od i = MoC.moore31 (psi41 ns) (psi11 od) (event i)
+moore32 ns od i = MoC.moore32 (psi41 ns) (psi12 od) (event i)
+moore33 ns od i = MoC.moore33 (psi41 ns) (psi13 od) (event i)
+moore34 ns od i = MoC.moore34 (psi41 ns) (psi14 od) (event i)
+moore41 ns od i = MoC.moore41 (psi51 ns) (psi11 od) (event i)
+moore42 ns od i = MoC.moore42 (psi51 ns) (psi12 od) (event i)
+moore43 ns od i = MoC.moore43 (psi51 ns) (psi13 od) (event i)
+moore44 ns od i = MoC.moore44 (psi51 ns) (psi14 od) (event i)
 
 
 mealy11 :: (st -> a1 -> st) -> (st -> a1 -> b1) -> st
@@ -249,30 +264,30 @@ mealy43 :: (st -> a1 -> a2 -> a3 -> a4 -> st) -> (st -> a1 -> a2 -> a3 -> a4 -> 
 mealy44 :: (st -> a1 -> a2 -> a3 -> a4 -> st) -> (st -> a1 -> a2 -> a3 -> a4 -> (b1, b2, b3, b4)) -> st
         -> Sig a1 -> Sig a2 -> Sig a3 -> Sig a4 -> (Sig b1, Sig b2, Sig b3, Sig b4)
 
-mealy11 ns od i = MoC.mealy11 (psi21 ns) (psi21 od) (value i)
-mealy12 ns od i = MoC.mealy12 (psi21 ns) (psi22 od) (value i)
-mealy13 ns od i = MoC.mealy13 (psi21 ns) (psi23 od) (value i)
-mealy14 ns od i = MoC.mealy14 (psi21 ns) (psi24 od) (value i)
-mealy21 ns od i = MoC.mealy21 (psi31 ns) (psi31 od) (value i)
-mealy22 ns od i = MoC.mealy22 (psi31 ns) (psi32 od) (value i)
-mealy23 ns od i = MoC.mealy23 (psi31 ns) (psi33 od) (value i)
-mealy24 ns od i = MoC.mealy24 (psi31 ns) (psi34 od) (value i)
-mealy31 ns od i = MoC.mealy31 (psi41 ns) (psi41 od) (value i)
-mealy32 ns od i = MoC.mealy32 (psi41 ns) (psi42 od) (value i)
-mealy33 ns od i = MoC.mealy33 (psi41 ns) (psi43 od) (value i)
-mealy34 ns od i = MoC.mealy34 (psi41 ns) (psi44 od) (value i)
-mealy41 ns od i = MoC.mealy41 (psi51 ns) (psi51 od) (value i)
-mealy42 ns od i = MoC.mealy42 (psi51 ns) (psi52 od) (value i)
-mealy43 ns od i = MoC.mealy43 (psi51 ns) (psi53 od) (value i)
-mealy44 ns od i = MoC.mealy44 (psi51 ns) (psi54 od) (value i)
+mealy11 ns od i = MoC.mealy11 (psi21 ns) (psi21 od) (event i)
+mealy12 ns od i = MoC.mealy12 (psi21 ns) (psi22 od) (event i)
+mealy13 ns od i = MoC.mealy13 (psi21 ns) (psi23 od) (event i)
+mealy14 ns od i = MoC.mealy14 (psi21 ns) (psi24 od) (event i)
+mealy21 ns od i = MoC.mealy21 (psi31 ns) (psi31 od) (event i)
+mealy22 ns od i = MoC.mealy22 (psi31 ns) (psi32 od) (event i)
+mealy23 ns od i = MoC.mealy23 (psi31 ns) (psi33 od) (event i)
+mealy24 ns od i = MoC.mealy24 (psi31 ns) (psi34 od) (event i)
+mealy31 ns od i = MoC.mealy31 (psi41 ns) (psi41 od) (event i)
+mealy32 ns od i = MoC.mealy32 (psi41 ns) (psi42 od) (event i)
+mealy33 ns od i = MoC.mealy33 (psi41 ns) (psi43 od) (event i)
+mealy34 ns od i = MoC.mealy34 (psi41 ns) (psi44 od) (event i)
+mealy41 ns od i = MoC.mealy41 (psi51 ns) (psi51 od) (event i)
+mealy42 ns od i = MoC.mealy42 (psi51 ns) (psi52 od) (event i)
+mealy43 ns od i = MoC.mealy43 (psi51 ns) (psi53 od) (event i)
+mealy44 ns od i = MoC.mealy44 (psi51 ns) (psi54 od) (event i)
 
 -- FILTER, FILL, HOLD
 
 
-when :: (a -> Bool) -- ^ Predicate function
+when :: Sig Bool    -- ^ Signal of predicates
      -> Sig a       -- ^ Input signal
      -> Sig a       -- ^ Output signal
-when p s = MoC.comb21 (replace Abst) (comb11 p s) s
+when p s = MoC.comb21 ( replace Abst) p s
 
 
 filter :: (a -> Bool) -- ^ Predicate function
@@ -284,5 +299,5 @@ fill :: a -> Sig a -> Sig a
 fill x s = MoC.comb21 (unsafeReplace (Value x)) (MoC.comb11 isAbsent s) s
 
 hold :: a -> Sig a -> Sig a
-hold init s = MoC.scanl11 fillF (value init) s
+hold init s = MoC.scanl11 fillF (event init) s
   where fillF st inp = unsafeReplace st (isAbsent inp) inp
