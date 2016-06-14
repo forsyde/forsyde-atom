96c4eaae34e5f74577ee9b972ad8172d970a32a1
ugeorge <ugeorge@kth.se>
2016-06-14 16:03:43 +0200

    texlive from ppa

--- tests/SYTest.hs
+++ tests/SYTest.hs
@@ -1,3 +1,8 @@
+module Main where
+
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Test.HUnit
 import Test.QuickCheck
 import ForSyDe
 import ForSyDe.Core.Signal
@@ -22,6 +27,18 @@ split c xs = xs' : if null xs'' then [] else split c (tail xs'')
 
 prop_split_inv xs = forAll (elements xs) $ \c -> unsplit c (split c xs) == xs
 
+   
+testfoo = TestCase $ assertEqual "Foo == Foo"
+    "Foo" "Foo"
+
+testfoo2 = TestCase $ assertEqual "Foo != Bar"
+    "Foo" "Bar"
+
+-- hUnitTestToTests: Adapt an existing HUnit test into a list of test-framework tests
+tests = hUnitTestToTests $ TestList [TestLabel "testfoo" testfoo,
+                                     TestLabel "testfoo2" testfoo2]
+
+
 
 -- fromSY (SY.SY a) = a
 
@@ -45,5 +62,7 @@ prop_split_inv xs = forAll (elements xs) $ \c -> unsplit c (split c xs) == xs
 -- sync_comb :: SY.Sig Int -> SY.Sig Int -> Bool 
 -- sync_comb sa sb = SY.comb21 (+) sa sb == signal ( map SY.SY $ zipWith (psi21 (+)) (map fromSY (fromSignal sa)) (map fromSY (fromSignal sb)))
 
-main = quickCheck prop_split_inv
+main = do
+  a <- quickCheck prop_split_inv
+  defaultMain tests
 -- /show
