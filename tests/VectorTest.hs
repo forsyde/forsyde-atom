6d6f9aa51a51a00d816559a904c1f30fa49be8c9
ugeorge <ugeorge@kth.se>
2016-10-22 21:39:25 +0200

    closes #17 . Added test cases and property tests for Vector

--- tests/VectorTest.hs
+++ tests/VectorTest.hs
@@ -58,6 +58,81 @@ prop_scatter_1 v1 v2 v3 = V.length (scatter v1 v2 v3) == V.length v2
   where types = (v1 :: Vector Integer, v2 :: Vector Int, v2 :: Vector Int)
 
 
+(v1,v2) = V.map12 (\x -> (x+1,x*2)) $ V.iterate 10 (+1) 3 :: (Vector Integer, Vector Integer)
+v3      = V.generate 5 id 1
+v4      = V.group 3 v1
+v5      = V.vector [1,3,5]
+
+test_map_1 = V.map11 (+1) v1 @?= V.vector [5,6,7,8,9,10,11,12,13,14]
+test_map_2 = V.map21 (+) v1 v2 @?= V.vector [10,13,16,19,22,25,28,31,34,37]
+
+test_reduce_1 = V.reduce1 (+) v1 @?= 85
+test_reduce_2 = V.reduce1' (+) 5 v1 @?= 90
+
+test_prefix_1 = V.prefix1 (+) v1 @?= V.vector [4,9,15,22,30,39,49,60,72,85]
+test_prefix_2 = V.prefix1' (+) 5 v1 @?= V.vector [9,14,20,27,35,44,54,65,77,90]
+
+test_suffix_1 = V.suffix1 (+) v1 @?= V.vector [85,81,76,70,63,55,46,36,25,13]
+test_suffix_2 = V.suffix1' (+) 5 v1 @?= V.vector [90,86,81,75,68,60,51,41,30,18]
+
+test_pipe_1 = V.pipe0 (fanoutn 5 V.tail) v1 @?= V.vector [9,10,11,12,13]
+
+test_systolic_1 = V.systolic0 (fanoutn 5 V.tail) v1 @?=
+  V.vector [V.vector [5,6,7,8,9,10,11,12,13],
+            V.vector [6,7,8,9,10,11,12,13],
+            V.vector [7,8,9,10,11,12,13],
+            V.vector [8,9,10,11,12,13],
+            V.vector [9,10,11,12,13]]
+
+test_cascade_1 = V.cascade0 (+) v3 v3 @?= V.vector [6,21,56,126,252]
+
+test_mesh_1 = V.mesh0 (+) v3 v3 @?=
+  V.vector [V.vector [2,3,4,5,6],
+            V.vector [3,6,10,15,21],
+            V.vector [4,10,20,35,56],
+            V.vector [5,15,35,70,126],
+            V.vector [6,21,56,126,252]]
+
+test_lenght_1    = V.length v1        @?= 10
+test_concat_1    = v1 V.<++> v2       @?= V.vector [4,5,6,7,8,9,10,11,12,13,6,8,10,12,14,16,18,20,22,24]
+test_concat_2    = V.concat v4        @?= V.vector [4,5,6,7,8,9,10,11,12,13]
+test_first_1     = V.first v1         @?= 4 
+test_last_1      = V.last v1          @?= 13
+test_init_1      = V.init v1          @?= V.vector [4,5,6,7,8,9,10,11,12] 
+test_tail_1      = V.tail v1          @?= V.vector [5,6,7,8,9,10,11,12,13]
+test_inits_1     = V.inits v3         @?= V.vector [V.vector [1],
+                                                    V.vector [1,1],
+                                                    V.vector [1,1,1],
+                                                    V.vector [1,1,1,1],
+                                                    V.vector [1,1,1,1,1]]
+test_tails_1     = V.tails v3         @?= V.vector [V.vector [1,1,1,1,1],
+                                                    V.vector [1,1,1,1],
+                                                    V.vector [1,1,1],
+                                                    V.vector [1,1],
+                                                    V.vector [1]]
+test_reverse_1   = V.reverse v1       @?= V.vector [13,12,11,10,9,8,7,6,5,4]
+test_group_1     = V.group 3 v1       @?= V.vector [V.vector [4,5,6],
+                                                    V.vector [7,8,9],
+                                                    V.vector [10,11,12],
+                                                    V.vector [13]]
+test_shiftr_1    = V.shiftr v1 1      @?= V.vector [1,4,5,6,7,8,9,10,11,12]
+test_shiftl_1    = V.shiftl v1 1      @?= V.vector [5,6,7,8,9,10,11,12,13,1]
+test_rotr_1      = V.rotr v1          @?= V.vector [13,4,5,6,7,8,9,10,11,12]
+test_rotl_1      = V.rotl v1          @?= V.vector [5,6,7,8,9,10,11,12,13,4]
+test_take_1      = V.take 4 v1        @?= V.vector [4,5,6,7]
+test_drop_1      = V.drop 4 v1        @?= V.vector [8,9,10,11,12,13]
+test_filterIdx_1 = V.filterIdx odd v1 @?= V.vector [4,6,8,10,12] 
+test_odds_1      = V.odds v1          @?= V.vector [4,6,8,10,12]
+test_evens_1     = V.evens v1         @?= V.vector [5,7,9,11,13]
+test_stride_1    = V.stride 2 4 v1    @?= V.vector [5,9,13]
+test_get_1       = v1 <@! 4           @?= 7
+test_gather_1    = V.gather1 v3 v1    @?= V.vector [4,4,4,4,4]
+test_replace_1   = V.replace 4 200 v1 @?= V.vector [4,5,6,200,8,9,10,11,12,13]
+test_scatter_1   = V.scatter v5 v3 v1 @?= V.vector [4,1,5,1,6]
+test_bitrev_1    = V.bitrev v1        @?= V.vector [11,7,9,13,5,10,6,8,12,4]
+test_duals_1     = V.duals v1         @?= V.vector [(4,9),(5,10),(6,11),(7,12),(8,13)]
+
+
 instance (Arbitrary a) => Arbitrary (Vector a) where
   arbitrary = do
     x <- arbitrary
